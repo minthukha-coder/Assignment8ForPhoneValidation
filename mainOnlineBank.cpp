0a63c8116ab34d0bb2a312ceade0fa6257dc65b2
Min Thu Kha <112501430+minthukha-coder@users.noreply.github.com>
2023-03-16 16:41:18 +0630

    Create mainOnlineBank.cpp

---
+++ mainOnlineBank.cpp
@@ -0,0 +1,17 @@
+
+//
+// Created by National Cyber City on 2/6/2023.
+//
+
+
+#include "stdio.h"
+#include "OnlineBankProject.h"
+
+int main(){
+
+    loadingAllDataFromFile();
+    printingAllData();
+    welcome();
+
+    return 0;
+}
